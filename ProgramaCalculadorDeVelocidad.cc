ce46dac339764a2b9b6781c4540c0c1947230325
AlejandraBonilla <31254521+AlejandraBonilla@users.noreply.github.com>
2017-11-10 13:09:47 -0600

    Create ProgramaCalculadorDeVelocidad.cc
    Programa que calcula la velcidad utilizando la fórmula básica: v=d/t

---
+++ ProgramaCalculadorDeVelocidad.cc
@@ -0,0 +1,21 @@
+// Examen: Alejandra Bonilla 
+// Programa que calcula la velocidad 
+#include <iostream>
+using namespace std;
+
+int main (){
+  float d;
+  float t; 
+  float v; 
+  
+  cout<<"Introduce el valor de la distancia en metros"<<endl;
+  cin>>d;
+  
+  cout<<"Introduce el valor del tiempo en segundos"<<endl;
+  cin>>t;
+  
+  cout<<"El valor de la velocidad es:\n"<<d/t<<"m/s"<<endl;
+
+ 
+  return 0;
+}
